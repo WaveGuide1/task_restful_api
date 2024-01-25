fa3da769a376397ed65b4037cc8984b623fe4f88
WaveGuide1 <barthalaekwe@gmail.com>
2024-01-25 13:34:00 +0100

    Routers and permission

--- house_app/models.py
+++ house_app/models.py
@@ -33,3 +33,6 @@ class House(models.Model):
     task_completed_count = models.IntegerField(default=0)
     task_uncompleted_count = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- house_app/views.py
+++ house_app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from .models import House
+from .serializer import HouseSerializer
+from .permission import IsManager
 
 # Create your views here.
 
 
-class HouseViewSet(viewsets):
-    pass
+class HouseViewSet(viewsets.ModelViewSet):
+    queryset = House.objects.all()
+    serializer_class = HouseSerializer
+    permission_classes = [IsManager]
+
