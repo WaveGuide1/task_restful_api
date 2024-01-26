7727bbc328c3ce636f4bfa59f63d06cf46d61d3c
WaveGuide1 <barthalaekwe@gmail.com>
2024-01-26 14:42:15 +0100

    tasklist and attachment model

--- task_app/models.py
+++ task_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import uuid
+import os
+from django.utils.deconstruct import deconstructible
 
 # Create your models here.
 COMPLETE = 'CP'
@@ -10,6 +13,30 @@ STATUS_CHOICE = [
     (NOT_COMPLETE, 'completed'),
     (PENDING, 'pending'),
 ]
+
+
+@deconstructible
+class FilePathGenerator(object):
+    def __init__(self):
+        pass
+
+    def __call__(self, instance, filename):
+        file_extension = filename.split('.')[-1]
+        filepath = f"media/task/{instance.task.id}/attachments/"
+        file_name = f"attach_file.{file_extension}"
+        return os.path.join(filepath, file_name)
+
+
+class TaskList(models.Model):
+    name = models.CharField(max_length=225)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(blank=True, null=True)
+    created_by = models.ForeignKey('user_app.userprofile', null=True, blank=True,
+                                   on_delete=models.SET_NULL, related_name='lists')
+    house = models.ForeignKey('house_app.House', on_delete=models.CASCADE,
+                              related_name='lists')
+    status = models.CharField(max_length=2, default=NOT_COMPLETE, choices=STATUS_CHOICE)
 
 
 class Task(models.Model):
@@ -25,3 +52,14 @@ class Task(models.Model):
 
     def __str__(self):
         return self.name
+
+
+file_path = FilePathGenerator()
+
+
+class Attachment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    task = models.ForeignKey('task_app.Task', on_delete=models.CASCADE,
+                             related_name='attachment')
+    file = models.FileField(upload_to=file_path, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
