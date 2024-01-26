27655002bce7f95f5849a3f1eb9db9bb2492d7f7
WaveGuide1 <barthalaekwe@gmail.com>
2024-01-26 12:30:14 +0100

    house app final update

--- house_app/views.py
+++ house_app/views.py
@@ -5,6 +5,7 @@ from .serializers import HouseSerializer
 from .permission import IsManager
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -14,7 +15,7 @@ class HouseViewSet(viewsets.ModelViewSet):
     serializer_class = HouseSerializer
     permission_classes = [IsManager]
 
-    @action(methods=['post'], detail=True, name='join', permission_classes=[])
+    @action(methods=['post'], detail=True, name='Join', permission_classes=[])
     def join(self, request, pk=None):
         try:
             house = self.get_object()
@@ -27,12 +28,12 @@ class HouseViewSet(viewsets.ModelViewSet):
                 return Response({'message': 'User is already a member of this house'},
                                 status=status.HTTP_400_BAD_REQUEST)
             else:
-                return Response({'message': 'User is already a member of this house'},
+                return Response({'message': 'User is already a member of another house'},
                                 status=status.HTTP_400_BAD_REQUEST)
         except Exception as err:
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    @action(methods=['post'], detail=True, name='leave', permission_classes=[])
+    @action(methods=['post'], detail=True, name='Leave', permission_classes=[])
     def leave(self, request, pk=None):
         try:
             house = self.get_object()
@@ -46,3 +47,22 @@ class HouseViewSet(viewsets.ModelViewSet):
                                 status=status.HTTP_400_BAD_REQUEST)
         except Exception as err:
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @action(methods=['post'], detail=True, name='Remove Member')
+    def remove(self, request, pk=None):
+        try:
+            house = self.get_object()
+            user_id = request.data.get('user_id', None)
+            if user_id is None:
+                return Response({'message': 'Action not allowed'}, status=status.HTTP_400_BAD_REQUEST)
+            user_profile = User.objects.get(pk=user_id).userprofile
+            house_members = house.members
+            if user_profile in house_members.all():
+                house_members.remove(user_profile)
+                house.save()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            else:
+                return Response({'message': 'User is not a member of this group'})
+        except Exception as err:
+            return Response({'message': 'Invalid credential'}, status=status.HTTP_400_BAD_REQUEST)
+
