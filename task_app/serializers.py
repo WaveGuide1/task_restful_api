087d938ac55a44915a95539b8bf7d2faaba9bebf
WaveGuide1 <barthalaekwe@gmail.com>
2024-01-28 03:32:38 +0100

    Task List serializer

---
+++ task_app/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Task, TaskList, Attachment
+from house_app.models import House
+
+
+class TaskListSerializer(serializers.ModelSerializer):
+    house = serializers.HyperlinkedRelatedField(queryset=House.objects.all(),
+                                                many=False, view_name='house-detail')
+    created_by = serializers.HyperlinkedRelatedField(read_only=True,
+                                                     many=False, view_name='userprofile-detail')
+
+    class Meta:
+        model = TaskList
+        fields = ['url', 'id', 'name', 'description',
+                  'created_by', 'status', 'house', 'created_at']
+        read_only_fields = ['created_by', 'status']
