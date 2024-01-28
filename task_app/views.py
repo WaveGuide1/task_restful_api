b9a29b604006114a03c926aef9e72cb3d33d8679
WaveGuide1 <barthalaekwe@gmail.com>
2024-01-28 03:49:04 +0100

    Task List Permission

--- task_app/views.py
+++ task_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from .models import TaskList, Task, Attachment
+from .serializers import TaskListSerializer
+from .permissions import IsTaskListCreator
 
 # Create your views here.
+
+
+class TaskListViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin,
+                      mixins.ListModelMixin, mixins.RetrieveModelMixin,
+                      mixins.DestroyModelMixin, mixins.UpdateModelMixin):
+    queryset = TaskList.objects.all()
+    serializer_class = TaskListSerializer
+    permission_classes = [IsTaskListCreator]
